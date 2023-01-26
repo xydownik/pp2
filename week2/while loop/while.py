e1597190a695bb460979b59271a3cbc04ec4768d
Aruzhan Sazanova <sazanovaa2@gmail.com>
2023-01-26 12:58:11 +0600

    First commit

---
+++ week2/while loop/while.py
@@ -0,0 +1,18 @@
+i=1
+while i<=10:
+    print(i)
+    i+=1
+j=1
+print("\n")
+while j<=10:
+    print(j)
+    if j == 5:
+        break
+    j+=1
+print("\n")
+k=1
+while k<=10:
+    k+=1
+    if k==5:
+        continue
+    print(k)
